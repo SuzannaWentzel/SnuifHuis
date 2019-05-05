cb53da51afede2e29198b630b0275a4fd62ba6fd
Suzanna Wentzel <suzannawentzel1708@gmail.com>
2019-05-05 21:41:13 +0200

    Created first part homepage, is responsive

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,36 +1,127 @@
 <template>
   <div id="app">
-    <div id="nav" class="nav-home">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-      <router-link
+    <div id="nav">
+      <div class="nav-block">
+        <router-link to="/">SNUIFHUIS</router-link>
+      </div>
+      <div class="nav-block">
+        <router-link to="/about">BEWONERS</router-link>
+      </div>
+      <div class="nav-block">
+        <router-link to="/" title="Food Wall of Shame">FWOS</router-link>
+      </div>
+      <div class="nav-block">
+        <router-link to="/">FOTO'S</router-link>
+      </div>
+      <div class="nav-block">
+        <router-link to="/">TITELS</router-link>
+      </div>
+      <div class="burger">
+        <i class="fas fa-hamburger" onclick="showNav()"></i>
+      </div>
     </div>
     <router-view />
-    <p>Hello world!</p>
   </div>
 </template>
 
+<script>
+  export default {
+    name: "App",
+    methods: {
+      showNav() {
+
+      }
+    }
+  };
+</script>
+
 <style lang="scss">
+  * {
+    padding: 0;
+    margin: 0;
+  }
+
+  @media screen and (max-width: 600px){
+    #nav {
+      z-index: 1000;
+      height: 60px;
+      width: 100%;
+      position: absolute;
+      background-color: rgba(0, 0, 0, 0.6);
+
+      .nav-block:not(:first-child) {
+        display: none;
+      }
+    }
+
+    .nav-block {
+      float: left;
+      padding-left: 20px;
+      font-size: 18px;
+      a {
+        color: white;
+        text-decoration: none;
+        line-height: 60px;
+      }
+    }
+
+    .burger {
+      color: white;
+      line-height: 60px;
+      font-size: 24px;
+      float: right;
+      padding-right: 20px;
+    }
+  }
+
+  @media screen and (min-width: 601px){
+    #nav {
+      height: 60px;
+      margin-top: 55vh;
+      width: 100%;
+      box-sizing: border-box;
+      z-index: 1000;
+      position: absolute;
+      display: flex;
+      align-content: center;
+      justify-content: center;
+      flex-flow: row nowrap;
+      font-family: segoe UI;
+      a {
+        font-weight: bold;
+      }
+    }
+
+    .nav-block {
+      width: 150px;
+      height: 40px;
+      margin-left: 0.5%;
+      margin-right: 0.5%;
+      margin-top: 10px;
+      background-color: rgba(255, 255, 255, 0.6);
+
+      a {
+        font-size: 18px;
+        line-height: 40px;
+        color: white !important;
+        text-decoration: none;
+        &.router-link-exact-active {
+          color: #2E0E02 !important;
+        }
+      }
+    }
+
+    .burger {
+      display: none;
+    }
+  }
+
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: "Segoe UI", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 
-  .nav-home {
-
-  }
 </style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="homepage">
     <div class="front-banner">
-      <h1>Welkom!</h1>
-      <h2>Op de <span class="highlighted">Snuifhuis</span> website</h2>
+      <div class="black">
+        <h1>Welkom!</h1>
+        <h2>Op de <span class="highlighted">Snuifhuis</span> website</h2>
+      </div>
     </div>
   </div>
 </template>
@@ -20,6 +22,20 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+  @media screen and (max-width: 600px){
+    .black {
+      padding-top: 30vh;
+    }
+  }
+
+  @media screen and (min-width: 601px){
+    .black {
+      padding: 30vh 0 0 0;
+      height: 70vh;
+    }
+  }
+
   .front-banner {
     background-image: url('../../public/images/woonkamer.jpg');
     height: 100vh;
@@ -29,5 +45,23 @@ export default {
     position: relative;
     background-position: center;
     background-size: cover;
+    display: block;
+    color: white;
+
+    h1 {
+      font-size: 75px;
+    }
+
+    h2 {
+      font-size: 26px;
+
+      .highlighted {
+        color: var(--yellow);
+      }
+    }
+  }
+
+  .black {
+    background-color: rgba(0, 0, 0, 0.5);
   }
 </style>
