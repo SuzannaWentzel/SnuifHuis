211a56869e1ccfdbe2effa0a560c8694e190dafa
Suzanna Wentzel <suzannawentzel1708@gmail.com>
2019-05-05 18:19:18 +0200

    Set up directory

---
+++ frontend/src/router.js
@@ -0,0 +1,22 @@
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home.vue";
+
+Vue.use(Router);
+
+function loadView(view) {
+  return () =>
+      import(`@/views/${view}.vue`);
+}
+
+export default new Router({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: loadView('Home')
+    }
+  ]
+});
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="homepage">
+    <div class="front-banner">
+      <h1>Welkom!</h1>
+      <h2>Op de <span class="highlighted">Snuifhuis</span> website</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "home",
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+  .front-banner {
+    background-image: url('../../public/images/woonkamer.jpg');
+    height: 100vh;
+    width: 100vw;
+    padding: 0;
+    margin: 0;
+    position: relative;
+    background-position: center;
+    background-size: cover;
+  }
+</style>
